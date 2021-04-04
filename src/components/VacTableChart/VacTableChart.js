7c619350d99f5e4690444a58fe04c545bd29549f
Steven Gadman <sgadman@users.noreply.github.com>
2021-04-03 21:50:37 -0400

    Update VacTableChart.js

--- src/components/VacTableChart/VacTableChart.js
+++ src/components/VacTableChart/VacTableChart.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(() => ({
     flexDirection: "row",
   },
   tableChartContainer: {
-    height: "calc(100vh - 142px",
+    height: "calc(100vh - 142px)",
   },
 }));
 
